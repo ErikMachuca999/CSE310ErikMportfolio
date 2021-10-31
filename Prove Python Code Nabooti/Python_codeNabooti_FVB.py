54cb97c47a57c60f472cbfb5117f9dfa2bbd1621
ErikMachuca999 <mac19024@byui.edu>
2021-10-31 00:31:40 -0500

    Module 3 Python Choose adventure

---
+++ Prove Python Code Nabooti/Python_codeNabooti_FVB.py
@@ -0,0 +1,314 @@
+import time
+# An intro scripted in fallout style, work in progress. Enjoy!
+time.sleep(2)
+print("Fallout: Van Buren")
+
+print()
+time.sleep(5)
+print('Someone\'s rocking my dreamboat...')
+print()
+time.sleep(5)
+print('    Someone\'s invading my dream...')
+print()
+time.sleep(5)
+print('      We were sailing along...')
+print()
+time.sleep(5)
+print('        So peaceful and calm...')
+print()
+time.sleep(5)
+print(' Suddenly, something went wrong...') 
+time.sleep(1)
+print()
+time.sleep(1)
+print()
+
+time.sleep(2)
+print('War. War never changes. The end of the world was abrupt.\n\
+All that was remembered was in one day, the world went from a\n\
+an everday routine to a new age of stone and ashes. Gone. In\n\
+an instant. That terrible, unsuspecting year was 2076')
+print()
+
+time.sleep(15)
+print('            _  . _  ,  _  .  _')
+print('          _  . _   _  ,   _  .  _')
+print("      ( _   '  (  `    ) _    . __ )")
+print("    (   (     (       )      ` )   ) _ )")
+print(" ( __   ( _      (_   .   _ )   _)  ,__ )")
+print("      `~ ~ ~` \ '   .    / `~ ~ ~`")
+print("               \        /     ")
+print("                ;      ;")
+print("               /        \   ")
+print("    _________/__  _____   \___________")
+print()
+
+time.sleep(3)
+print('Those that were lucky enough, had been blessed to make it\n\
+in large underground shelters, known as vaults. Named under a rather\n\
+inconspicuous company that appeared almost out of nowhere, these vaults\n\
+saved a considerable amount of survivors. Others were not so fortunate.\n\
+What is left of them would say exactly what transpired.')
+
+print()
+
+time.sleep(20)
+print('a rather selected few narrowly escaped the fiery holocaust. In secret\n\
+they have connections within themselves and in the end, have abandoned their kind to\n\
+their ill gotten fate. Misguided by dastardly philosophy, they began to reestablish\n\
+the remains of what was left of the past broken United States government; renaming a\n\
+regrown and "rewritten" faction known as The Enclave.')
+
+print()
+
+time.sleep(22)
+print('Throughout many years, they kept silent. They just watched, and studied in secret\n\
+of the "new situation", they called it, to learn how to adapt and live up to the now\n\
+scorched radiated Wasteland.')
+print()
+
+time.sleep(19)
+
+print('The Enclave emerged in 2241. tales throughout the western half of the\n\
+North American continent, during the old world, known as The sates of Califronia,\n\
+Oregon, and Nevada began spreading about their infamy. Inhumane treatment of their\n\
+fellow man. "Irradiated" meant nothing to them, only termination or "Science"')
+
+print()
+
+time.sleep(26)
+print('A heartless agenda involed The Enclave locating survivors from the appocalypse,\n\
+wherever they were and performed all types of biological tests on them. From this, they have\n\
+created a terrifying super virus, designed to destroy all irradiated life, friend or enemy, \n\
+leaving the pure that cheated death, or were more fortunate unscathed. A plan to utilize this\n\
+virus was to release it globally. Their "cleansing" would have succeded. If it wasn\'t for an\n\
+ambiguous individual; one that was universally known from the tales from the wasteland:\n\
+The Chosen One.')
+
+print()
+
+time.sleep(33)
+print('The actions of The Chosen One grew to the status of legend, with their travels going around\n\
+post-appocalyptic California and Oregon. The Chosen One\'s path crossed with The Enclave and the\n\
+result was determined. The Enclave lost. The wasteland rested more easily thanks to the heroics of\n\
+this mysterious wastelander. 11 years have past after The Enclave lost their plans, their Virus, and\n\
+Most of their followers.')
+
+print()
+
+time.sleep(20)
+print('The Enclave dispersed to the east coast of America, fighting to restabalize their hurt group.\n\
+As all may know, Human nature is always stubborn. The peace from evil was only momentarily, as a few \n\
+survivors from the dispersed enclave decided to stay behind in the now united west coast, called The New\n\
+California Republic. They mindfully stayed out of sight as they salvaged past research from their super\n\
+virus, linking to past medical history. These "Enclave remnants" now have access to files to a past danger\n\
+ready to enact revenge, determined to continue the agenda of their " New American Dream"..... ')
+
+time.sleep(24)
+print()
+
+time.sleep(2)
+print()
+#|---------------------------------------SEPARATOR---LINE------------------------------------------------------
+#scene 1
+print()
+time.sleep(3)
+print('2254     Location: A prison')
+print()
+
+time.sleep(3)
+print('A dark, corridor sparks up to life with dim light...')
+print()
+time.sleep(5)
+print('A person is shown slumping against a wall...')
+print()
+time.sleep(5)
+print('the person is seen knocked out but with no injuries...')
+print()
+time.sleep(5)
+print('then a sound of clicking broke silence...')
+print()
+time.sleep(5)
+print('static hisses then what is heard is an old song...')
+time.sleep(5)
+print()
+print('the person gasps for air and tenses up their body with breath...')
+time.sleep(5)
+print()
+print('the person\'s eyes blink hard and bows their head down away from the light...')
+time.sleep(8)
+print()
+print('This person is...You. ')
+time.sleep(2)
+print()
+
+time.sleep(2)
+print('You look around in the room which you are in. you see everything in\n\
+dim cyan light. You widen your eyes to the fact that you see bars encircling\n\
+around your room. you then see a bare steel barred bed near a thick glass showing\n\
+shining light to the outside. you then jump a little to the realzation of a peculiar\n\
+sound. you see an old radio near your bed from where you are sitting against the wall.')
+print()
+
+time.sleep(21)
+print('You listen in on what the radio was playing. you recognize a few of the words\n\
+from the speaker. It\'s a familiar song; definetly ink spots, you think to yourself.\n\
+You grunt loudly and tilt your neck to see around your back, to the fact of you can\'t\n\
+move your arms or hands. Shackles! you see your attire in more detail and get confused\n\
+as you see now where you are. You are in a prison.')
+print()
+
+time.sleep(21)
+print('You let out a yelp as you then hear someone call out to you; the sound of a voice!\n\
+you look around and start thinking you are loosing your mind. You see a thing, not a person.\n\
+It took you a few good seconds to recognize what the thing is: a robot. A securitron to be exact.\n\
+a weird looking robot with a TV for a face, and has its plated body fastened on a strong rod\n\
+attached to a big tire wheel.')
+print()
+
+time.sleep(20)
+print('The securitron talks to you about where you are. You are confined here in what is The\n\
+Tibbets Prison. It goes on about how escape is useless and brags about the security. The bot then\n\
+instructs you to not try anything as shortly you are about to be escorted to an "orientation" and\n\
+tells you to stay put. It then leaves. ')
+print()
+
+time.sleep(20)
+print('Questions flood your mind. Why? How? When?... you are quick decide that you don\'t want\n\
+to be here anymore and did your best to remain calm. You then notice that the shackles you have\n\
+are old fashioned. you are a good lockpicker and thought that you could snap the cuffs in a\n\
+snap. you search around for something to fashion into a lockpick.')
+print()
+
+time.sleep(12)
+print('you soon scrounge up a weird looking paper clip. while you feel around the clip, you think\n\
+about something else, you remember the way the securitron moves. you could rather easily knock him\n\
+with your legs with a specific move you know. you must make a decision as you feel that you used up\n\
+much of time before the orientation.')
+print()
+
+print('What do you wish to do?')
+print('Do you wish to try and lockpick the shackles? or \
+comply with the securitron\'s orders? ')
+
+choice1 = input('(X)LOCKPICK or (A)COMPLY? ')
+if choice1.upper() == 'X':
+    print()
+    print('You twist around the lockpick you made and after a few minutes, you hear a loud click.\n\
+The shackles are off you! but then you hear another beeping from down the hall. The beeping\n\
+gets louder and louder near to were you are. You then see a floating mechanical orb floating\n\
+in front of your barred cell. Then the orb swiftly shot out a beam at your face and hit you\n\
+in the eyes. Directly. The Securitron then arrives a few minutes later seeing you shot down.')
+    print('The securitron grumbles at how stubborn humans are. As you see your time in prison\n\
+was short.')
+    print('                                   GAME OVER')
+    print('Try Again.')
+    choice1_again = input('(X)LOCKPICK or (A)COMPLY? ')
+    if choice1_again.upper() == 'X':
+        print('NO. as punishment. watch the intro again.')
+
+#|---------------------------------------SEPARATOR---LINE------------------------------------------------------
+elif choice1.upper() == 'A':
+    print()
+    print('You slide the paper clip under your bed and decide to wait for the securitron\n\
+to return. It does and you wait for him to escort you. as you wait for its back turned\n\
+away from you, you launch yourself onto the robot pushing him down. You crash open its\n\
+screen and found many pieces to use unlocking your shackles. you make it to the cafereria')
+    print()
+    print('After you are free, an attack on the prison happens and makes a huge hole in the\n\
+building! you here the alarms and see many of the prison gaurd ROBOTS coming down the hall.\n\
+what to do?')
+    print()
+#--------------------------------------------------------------------------------------------------------------    
+    choice2 = input('(X)TAKE COVER? (A)MAKE A RUN FOR FREEDOM? or (Z)FIGHT THE BOTS? ')
+    if choice2.upper() == 'X':
+        print()
+        print('+5 to sneak skill, and utilized being under the tables and got close to the\n\
+hole leading outside. you realize that you need your gear you had before being\n\
+captured.')
+        print()
+
+#|-------------------------------------------------------------------------------------------------------------
+        choice3 = input('(Z)GO GET GEAR? or (C)GET OUT NOW? ')
+        if choice3.upper() == 'Z':
+            print()
+            print('you narrowly avoided recapture and repossed your important belongings.')
+        elif choice3.upper() == 'C':
+            print()
+            print('You escape with just the clothes on you back. But a tri pod robot\n\
+then activates within the prison, targeting you with the profile kept\n\
+secret. The robot locks on to you and starts following you.  THE END.')
+            print()
+#|-------------------------------------------------------------------------------------------------------------
+    elif choice2.upper() == 'A':
+        print('on no! you got caught in an unexpected crossfire and died.')
+#|-------------------------------------------------------------------------------------------------------------
+    elif choice2.upper() == 'Z':
+        print('what?! as you fought some bots, you can put on there broken metal and duisguise yourself!')
+        print('You got out with a new robot disguise!')
+#|---------------------------------------SEPARATOR---LINE------------------------------------------------------
+else:
+    print('Invalid input, please just type the letter of your choice')
+    re_choice1 = input('(X)LOCKPICK or (A)COMPLY? ')
+#|-------------------------------------------------------------------------------------------------------------
+    if re_choice1.upper() == 'X':
+        print('You twist around the lockpick you made and after a few minutes, you hear a loud click.\n\
+The shackles are off you! but then you hear another beeping from down the hall. The beeping\n\
+gets louder and louder near to were you are. You then see a floating mechanical orb floating\n\
+in front of your barred cell. Then the orb swiftly shot out a beam at your face and hit you\n\
+in the eyes. Directly. The Securitron then arrives a few minutes later seeing you shot down.')
+        print('The securitron grumbles at how stubborn humans are. As you see your time in prison\n\
+was short.')
+        print('                                   GAME OVER')
+        print('Try Again.')
+        choice1_again = input('(X)LOCKPICK or (A)COMPLY? ')
+        if choice1_again.upper() == 'X':
+            print('NO. as punishment. watch the intro again.')
+
+#|---------------------------------------SEPARATOR---LINE------------------------------------------------------
+    elif re_choice1.upper() == 'A':
+        print()
+        print('You slide the paper clip under your bed and decide to wait for the securitron\n\
+to return. It does and you wait for him to escort you. as you wait for its back turned\n\
+away from you, you launch yourself onto the robot pushing him down. You crash open its\n\
+screen and found many pieces to use unlocking your shackles. you make it to the cafereria')
+        print()
+        print('After you are free, an attack on the prison happens and makes a huge hole in the\n\
+building! you here the alarms and see many of the prison gaurd ROBOTS coming down the hall.\n\
+what to do?')
+        print()
+        choice2 = input('(X)TAKE COVER? (A)MAKE A RUN FOR FREEDOM? or (Z)FIGHT THE BOTS? ')
+    if choice2.upper() == 'X':
+        print()
+        print('+5 to sneak skill, and utilized being under the tables and got close to the\n\
+hole leading outside. you realize that you need your gear you had before being\n\
+captured.')
+        print()
+        choice3 = input('(Z)GO GET GEAR? or (C)GET OUT NOW? ')
+        if choice3.upper() == 'Z':
+            print()
+            print('you narrowly avoided recapture and repossed your important belongings.')
+        elif choice3.upper() == 'C':
+            print()
+            print('You escape with just the clothes on you back. But a tri pod robot\n\
+then activates within the prison, targeting you with the profile kept\n\
+secret. The robot locks on to you and starts following you.  THE END.')
+            print()
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+     
+   
+     
+    
---
+++ Prove Python Code Nabooti/README.md
@@ -0,0 +1,24 @@
+# Overview
+
+This is a small and sadly cut down version of a Choose your own adventure program made in Python. 
+Heavily Inspired story from the Fallout Franchise, the cut story of The Original Fallout 3 entry
+for the franchise: "Van buren". I thought of using functions for it to make less tedious, but
+because of many ideas and to make it as quick as possible, I had to minimize the story and 
+right out at least the endings. But all works, and made a fun time making a cut story real 
+again. Used by if and else statemnets, and the time library
+
+
+[Watch my video demonstration Here](https://youtu.be/op1mco8YCyc)
+
+# Development Environment
+
+* Visual Studio Code
+* Python 3.8.6 64-bit
+* Git / GitHub
+
+# Useful Websites
+
+{Make a list of websites that you found helpful in this project}
+* [docs.python.org - Time access and conversions](https://docs.python.org/3/library/time.html)
+* [(used as quick reference) Choose Your Own Adventure Game in Python (Beginners)](https://www.youtube.com/watch?v=DEcFCn2ubSg)
+
